f6abcb385140df52350985df48e04ccd2a774ac9
Jongjun Park <pjjkey@kaist.ac.kr>
2020-08-24 00:58:15 +0900

    sample hello world

--- train.py
+++ train.py
@@ -9,7 +9,9 @@ import aicrowd_helper
 import gym
 import minerl
 import numpy as np
-import tensorflow.keras as keras
+from tensorflow.keras.models import Model
+from tensorflow.keras import layers
+from tensorflow.keras import Input
 from collections import deque
 import random
 from utility.parser import Parser
@@ -28,7 +30,7 @@ MINERL_TRAINING_MAX_INSTANCES = int(os.getenv('MINERL_TRAINING_MAX_INSTANCES', 5
 # Round 2: Training timeout is 4 days
 MINERL_TRAINING_TIMEOUT = int(os.getenv('MINERL_TRAINING_TIMEOUT_MINUTES', 4*24*60))
 # The dataset is available in data/ directory from repository root.
-os.environ['MINERL_DATA_ROOT'] = 'C:\Users\J.J.Park\PycharmProjects\MineRL\data\MineRLObtainDiamondVectorObf-v0'
+os.environ['MINERL_DATA_ROOT'] = 'data/'
 MINERL_DATA_ROOT = os.getenv('MINERL_DATA_ROOT', 'data/')
 
 # Optional: You can view best effort status of your instances with the help of parser.py
@@ -49,8 +51,8 @@ MIN_REPLAY_SIZE = 5000
 RUN_EPISODE_NUM = 25000
 MIN_EPISODE_NUM = 1000
 
-EPSILON_I = 1.0
-EPSILON_DECAY = 1/(RUN_EPISODE_NUM-MIN_EPISODE_NUM)
+EPSILON_I = 0
+EPSILON_DECAY = 0.1/(RUN_EPISODE_NUM-MIN_EPISODE_NUM)
 EPSILON_MIN = 0.1
 
 LEARNING_RATE = 0.00025
@@ -60,13 +62,16 @@ TARGET_UPDATE_FREQ = 10000
 
 MINIBATCH_SIZE = 32
 
-PRINT_INTERVAL = 100
+PRINT_FREQ = 10
 #MINIBATCH_STEP_SIZE =
 
 SAVE_FREQ = 1000
 MAIN_MODEL_PATH = 'train/main_model'
 TARGET_MODEL_PATH = 'train/target_model'
 
+#constants for use
+POV_INPUT_SHAPE = (64, 64, 3)
+VECTOR_INPUT_SPACE = (64,)
 NUM_ACTION_SPACE = 64
 LOW = -1.0499999523162842
 HIGH = 1.0499999523162842
@@ -107,26 +112,38 @@ class Agent:
 
         self.epsilon = epsilon_i
 
-    #create CNN network for Q-value
+    #create CNN network to get Q-value
     def intrinsic_create_model(self):
-        model = keras.Sequential()
-        model.add(keras.layers.Conv3D(8, 8, padding='same', input_shape=(64, 64, 64, 3), activation='relu'))
-        model.add(keras.layers.MaxPool3D((2, 2, 2)))
-        model.add(keras.layers.Dropout(0.1))
-        model.add(keras.layers.Conv3D(16, 4, padding='same', activation='relu'))
-        model.add(keras.layers.MaxPool3D((2, 2, 2)))
-        model.add(keras.layers.Conv3D(16, 2, padding='same', activation='relu'))
-        model.add(keras.layers.MaxPool3D((2, 2, 2)))
-        model.add(keras.layers.Conv3D(16, 2, padding='same', activation='relu'))
-        model.add(keras.layers.MaxPool3D((2, 2, 2)))
-        model.add(keras.layers.Dropout(0.25))
-        model.add(keras.layers.Conv3D(32, 2, padding='same', activation='relu'))
-        model.add(keras.layers.Flatten())
-        model.add(keras.layers.Dense(96, kernel_initializer='normal', activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dense(NUM_ACTION_SPACE, kernel_initializer='normal', activation='softmax'))
 
-        model.compile(optimizer='rmsprop', loss=keras.losses.CategoricalCrossentropy())
+        pov_input = Input(shape=POV_INPUT_SHAPE, name='pov')
+        conv2d_layer_1 = layers.Conv2D(8, 8, padding='same', input_shape=POV_INPUT_SHAPE, activation='relu')(pov_input)
+        maxpool_layer_1 = layers.MaxPool2D((2, 2))(conv2d_layer_1)
+        dropout_1 = layers.Dropout(0.1)(maxpool_layer_1)
+        conv2d_layer_2 = layers.Conv2D(16, 4, padding='same', activation='relu')(dropout_1)
+        maxpool_layer_2 = layers.MaxPool2D((2, 2))(conv2d_layer_2)
+        dropout_2 = layers.Dropout(0.1)(maxpool_layer_2)
+        conv2d_layer_3 = layers.Conv2D(16, 2, padding='same', activation='relu')(dropout_2)
+        maxpool_layer_3 = layers.MaxPool2D((2, 2))(conv2d_layer_3)
+        dropout_3 = layers.Dropout(0.2)(maxpool_layer_3)
+        conv2d_layer_4 = layers.Conv2D(16, 2, padding='same', activation='relu')(dropout_3)
+        maxpool_layer_4 = layers.MaxPool2D((2, 2))(conv2d_layer_4)
+        dropout_4 = layers.Dropout(0.25)(maxpool_layer_4)
+        conv2d_layer_5 = layers.Conv2D(32, 2, padding='same', activation='relu')(dropout_4)
+        flatten = layers.Flatten()(conv2d_layer_5)
+        pov_dense = layers.Dense(64, kernel_initializer='normal', activation='relu')(flatten)
+
+        vector_input = Input(shape=VECTOR_INPUT_SPACE, name='vector')
+        vector_dense_1 = layers.Dense(128, activation='relu')(vector_input)
+        vector_dense_2 = layers.Dense(128, activation='relu')(vector_dense_1)
+
+        concatenated = layers.concatenate([pov_dense, vector_dense_2])
+
+        batch_normalized = layers.BatchNormalization()(concatenated)
+        determined_action = layers.Dense(NUM_ACTION_SPACE, kernel_initializer='normal', activation='softmax')(batch_normalized)
+
+        model = Model([pov_input, vector_input], determined_action)
+
+        model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
         model.summary()
 
         return model
@@ -170,20 +187,25 @@ class Agent:
         loss = hist.history['loss'][0]
         return loss
 
-
     #determining action
     def get_action(self, state):
         if self.epsilon > np.random.rand():
             rand_action = {
-                'vector' : np.random.randint(low=LOW, high=HIGH, size=NUM_ACTION_SPACE)
+                'vector': np.random.uniform(low=LOW, high=HIGH, size=NUM_ACTION_SPACE)
             }
             return rand_action
         else:
-            index = np.argmax(self.main_model.predict(state))
+            pov = np.reshape(np.array(state['pov']), (1, 64, 64, 3))
+            vector = np.reshape(np.array(state['vector']), (1, 64))
+            input_state = {'pov': pov, 'vector': vector}
+            predicted_vector = self.main_model.predict(input_state)
+            predicted_vector = np.ravel(predicted_vector).astype(np.float32)
+            #temp_vector = predicted_vector-((np.max(predicted_vector)-np.min(predicted_vector))/2)
+            #norm = (HIGH-LOW)/(np.max(predicted_vector)-np.min(predicted_vector))
+            #result_vector = temp_vector * norm
             trained_action = {
-                'vector' : np.zeros((1,NUM_ACTION_SPACE))
+                'vector': predicted_vector
             }
-            trained_action[index] = HIGH
             return trained_action
 
     #increase target update counter & update target network if necessary
@@ -207,14 +229,14 @@ def main():
     """
     # How to sample minerl data is document here:
     # http://minerl.io/docs/tutorials/data_sampling.html
-    data = minerl.data.make(MINERL_GYM_ENV, data_dir=MINERL_DATA_ROOT)
+    data = minerl.data.make(environment=MINERL_GYM_ENV)
 
     # Sample code for illustration, add your training code below
     env = gym.make(MINERL_GYM_ENV)
     #env.make_interactive(port=31415, realtime=True)
 
     #MY_CODE_BELOW_HERE
-    obs = env.reset()
+    current_state = env.reset()
     done = False
 
     agent = Agent(REPLAY_MEMORY_SIZE, MIN_REPLAY_SIZE,
@@ -223,46 +245,45 @@ def main():
                            TARGET_UPDATE_FREQ, MINIBATCH_SIZE)
 
     step = 0
+    episode_reward = 0
     rewards = []
     losses = []
 
-    #training for RUN_EPISODE_NUM
-    for episode in range(RUN_EPISODE_NUM):
+    aicrowd_helper.training_start()
 
-        state = obs
+    #one episode per a loop.
+    while not done:
+
+        step += 1
         episode_reward = 0
-        done = False
-        #one episode per a loop.
-        while not done:
-            step += 1
 
-            #determine action and apply action in MineRl env
-            action = agent.get_action(state)
-            obs, rew, halt, _ = env.step(action)
+        #determine action and apply action in MineRl env
+        action = agent.get_action(current_state)
+        next_state, reward, done, _ = env.step(action)
 
-            # information aquisition
-            next_state = obs
-            reward = rew
-            episode_reward += reward
-            done = halt
+        # information aquisition
+        episode_reward += reward
 
-            #in training mode, store data in replay memory
-            agent.update_replay_memory(state, action, reward, next_state, done)
-            #state update(in this case 'pov')
-            state = next_state
+        #in training mode, store data in replay memory
+        agent.update_replay_memory(current_state, action, reward, next_state, done)
+        #state update(in this case 'pov')
+        current_state = next_state
 
-            if episode > MIN_EPISODE_NUM:
-                loss = agent.train(done)
-                losses.append(loss)
+        loss = agent.train(done)
+        losses.append(loss)
 
-                #updating target network
-                if step%TARGET_UPDATE_FREQ == 0:
-                    agent.update_target_network()
+        #updating target network
+        if step%TARGET_UPDATE_FREQ == 0:
+            agent.update_target_network()
+
+    rewards.append(episode_reward)
 
 
-        rewards.append(episode_reward)
-        if step%SAVE_FREQ==0 and step!=0:
-            agent.save(MAIN_MODEL_PATH, TARGET_MODEL_PATH)
+    agent.save(MAIN_MODEL_PATH, TARGET_MODEL_PATH)
+
+
+    print("step: {} / reward: {:.2f} / loss: {:.4f}"
+        .format(step, np.mean(rewards), np.mean(losses)))
 
 
 #     actions = [env.action_space.sample() for _ in range(10)] # Just doing 10 samples in this example
